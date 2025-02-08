778d43c7f095429b89d19c07eed9e18b4bbc6672
key <54595444+fowwo@users.noreply.github.com>
2025-02-07 20:07:10 -0600

    Add daily challenge room fetch

--- Data/fetch.ts
+++ Data/fetch.ts
@@ -1,3 +1,5 @@
+import type { APIRoom } from "./Types/API/Room";
+
 /** Fetches from the API with default headers and error handling. */
 export async function fetchAPI(endpoint: string, token: string, init?: RequestInit) {
 	const res = await fetch("https://osu.ppy.sh/api/v2/" + endpoint, {
@@ -11,4 +13,11 @@ export async function fetchAPI(endpoint: string, token: string, init?: RequestIn
 
 	if (!res.ok) throw new Error(`Failed to fetch API: ${res.status} - ${res.statusText}. ${JSON.stringify(await res.json())}`);
 	return await res.json();
+}
+
+/** Fetches the daily challenge rooms and returns the result as provided by the API. */
+export async function fetchAPIRooms(token: string, limit?: number) {
+	return await fetchAPI("rooms?category=daily_challenge&mode=all" + (limit ? `&limit=${limit}` : ""), token, {
+		headers: { "x-api-version": "20240529" }
+	}) as APIRoom[];
 }
